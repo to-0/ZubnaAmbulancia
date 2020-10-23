4ed397a75512d1e3a65803fcbcb72b56f4a127e7
Tomas Kalny <xkalny@stuba.sk>
2020-10-23 21:07:23 +0200

    Prvy

---
+++ src/employees/DentalAssistant.java
@@ -0,0 +1,12 @@
+package employees;
+
+import model.Person;
+
+public class DentalAssistant extends Person {
+
+	public DentalAssistant(String firstn, String secondN, int age, String phone_num) {
+		super(firstn, secondN, age, phone_num);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/model/Person.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Person {
+	private String firstName;
+	private String secondName;
+	private int age;
+	private String phone_number;
+	public Person(String firstn,String secondN, int age, String phone_num) {
+		this.firstName=firstn;
+		this.secondName = secondN;
+		this.age = age;
+		this.phone_number = phone_num;
+		
+	}
+}
